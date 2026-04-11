e9b4594a501d9541b7050d59adb094e91db99b71
abhimanyujoshi15 <abhimanyujoshi2005@gmail.com>
2026-04-11 09:42:58 +0530

    updated index.js

--- server/index.js
+++ server/index.js
@@ -17,7 +17,11 @@ const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 
 // Routes
